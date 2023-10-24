a76c21befcd4a518ee371810c9b0ab1013811d71
hast06001 <hast06001@E-PW024NW1>
2023-10-24 20:16:27 +0200

    Add project files.

---
+++ Uppgift 4.12/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Uppgift_4._12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bestäm kvadraternas sidlängd.");
+            int sidlängd = int.Parse(Console.ReadLine());
+            Console.WriteLine("Bestäm rutnätets bredd.");
+            int bredd = int.Parse(Console.ReadLine());
+            Console.WriteLine("Bestäm rutnätets höjd.");
+            int hojd = int.Parse(Console.ReadLine());
+
+            for(int i = 0; i < hojd; i++)
+            {
+                for(int j = 0; j < bredd; j++)
+                {
+                    for (int k = 0; k < sidlängd; k++)
+                        Console.Write("*");
+                    Console.Write(" ");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
